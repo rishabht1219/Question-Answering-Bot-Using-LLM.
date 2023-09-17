72d162c2edd8a0fcf4aa32b1a7acf835aea8c636
Rishabh Tiwari <66384501+rishabht1219@users.noreply.github.com>
2023-09-17 21:40:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'private_key.json'
 
 options = dict()
 
-options['api_keys'] =  'AIzaSyDrNsO47enZNTf_1GWo8KuTfqiqNo0k7Fg' # for knowledge graph extraction
+options['api_keys'] =  'Your google search API key' # for knowledge graph extraction
 options['headers'] = {'User-Agent': 'Mozilla/5.0'}
 options["log_path"] =  "logs/test.log"
